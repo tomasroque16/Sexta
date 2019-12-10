794eb7a8b0c4f3f1325d591f3c83d0d732d004bb
tomasroque16 <tomasmedeirosroque@enta.pt>
2019-12-10 11:34:33 -0100

    Ficheiro irs

---
+++ irs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (void) {
+   char nome[250]
+   char estadocivil[1]
+        printf("Inserir o seu nome a:\n->");
+            scanf("%d", &nome);
+        printf("Inserir seu Estado civil b:\n->");
+            scanf("%d", &estadocivil);
+
+             /* Estado Civil */
+
+    if (civ="S"  || civ="s" ){
+    
+    else if (civ="C" || civ="c")
+    else if (estadocivil="U" || estadocivil="u")
+    else if (estadocivil="D" || estadocivil="d")
+    
+        
+    }
+    
+    
+}
+    
+    
