1b603ea233a3a9fe159930ee7ba5b3a43224e4a4
tomasroque16 <tomasmedeirosroque@enta.pt>
2020-01-06 09:48:23 -0100

    ficheiro prog1

---
+++ estadocivilnome.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main () {
+   char estadocivil = 'B';
+   switch(estadocivil)
+      case 'C' :
+         printf("Casado!\n" );
+         break;
+      case 'S' :
+         printf("Solteiro\n" );
+         break;
+      case 'D' :
+         printf("Divorciado\n" );
+         break;
+      case 'V' :
+         printf("Viuvo\n" );
+         break;
+      default :
+         printf("Invalido \n" );
+   }
+   
+   printf("Your grade is  %c\n", grade );
+ 
+   return 0;
--- progr4.c
+++ progr4.c
@@ -6,9 +6,13 @@ int main (void) {
     float num2;
     double soma;
     
-        printf("Insira numero: \n");
+        printf("Insira o primeiro numero: \n");
         scanf("%lf"; &lst);
         printf("Valor total = %lf\n" , num1);
         scanf("%lf": &lst);
-    soma = s
+
+    soma = (num1+num2)
+
+    printf("Valor n1 = %d e valor n2 = %d, soma = %d\n", num1, num2,");
+    
 
